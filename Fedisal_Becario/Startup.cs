0c6a202320bdb1bec9457bd680d6f5d32b79b3e1
diegolem <30736865+diegolem@users.noreply.github.com>
2018-04-20 23:56:23 -0600

    Configuraciones iniciales
    - Arreglando diseños y entity model

---
+++ Fedisal_Becario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fedisal_Becario.Startup))]
+namespace Fedisal_Becario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
